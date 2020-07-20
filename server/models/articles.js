8d1a62e1c341e9f9ee9b61c50997b1b254c95ebe
Meghan Mueller-Cox <mlm25@pdx.edu>
2020-07-20 14:57:02 -0700

    parameterized existing articles queries, renamed rating field to rating_sum, added rating_count field, modified articles apis one for inserting and one for updating an existing record's rating

--- server/models/articles.js
+++ server/models/articles.js
@@ -2,7 +2,8 @@ const db = require('../database');
 
 class Articles {
   static retrieveAll (callback) {
-    db.query('SELECT name FROM articles', (err, res) => {
+    let text = 'SELECT * FROM articles';
+    db.query(text, (err, res) => {
       if (err.error)
         return callback(err);
       callback(res);
@@ -10,7 +11,25 @@ class Articles {
   }
 
   static insert (article, callback) {
-    db.query('INSERT INTO articles (name) VALUES ($1)', [article], (err, res) => {
+    let text = 'INSERT INTO articles (url, name, source, date, rating_sum, rating_count) VALUES ($1, $2, $3, $4, $5, $6)';
+    let name = article.name;
+    let url = article.url;
+    let source = article.source;
+    let date = article.date;
+    let rating = article.rating;
+    let count = 1;
+    db.query(text, [name, url, source, date, rating, count], (err, res) => {
+      if (err.error)
+        return callback(err);
+      callback(res);
+    });
+  }
+
+  static update (article, callback) {
+    let text = `UPDATE articles SET rating_sum = rating_sum + $2, rating_count = rating_count+1 WHERE url=$1`;
+    let url = article.url;
+    let rating = article.rating;
+    db.query(text, [url, rating], (err, res) => {
       if (err.error)
         return callback(err);
       callback(res);
