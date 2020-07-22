5506cfd3ed381b1616aa0540b2519856a31d0439
Connor Robetorye <crobetorye@gmail.com>
2020-07-22 00:37:42 -0700

    lifted label for select-date

--- client/src/components/state-view.component.js
+++ client/src/components/state-view.component.js
@@ -230,8 +230,8 @@ export default class StateView extends Component {
                                         <option value="WY">Wyoming</option>
                                     </Input>
                                 </FormGroup>
-                                <FormGroup>
-                                    <Label for="select-date">Select Date Range:</Label>
+                                <Label for="select-date">Select Date Range:</Label>
+                                <FormGroup id="select-date">
                                     {/* issues: selected value doesn't clear if changing month menu
                                                 in middle of selection. */}
                                     {/* Ref: https://reactdatepicker.com/ */}
