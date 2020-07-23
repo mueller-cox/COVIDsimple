e406d41bb00d8314d1d4e936fb00b4a8df244bc5
Meghan Mueller-Cox <mlm25@pdx.edu>
2020-07-22 19:35:36 -0700

    adding news feed articles to news ui and temporarily removed abstract section

---
+++ client/src/components/feed-articles.component.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Table, Button, ButtonDropdown, DropdownItem, DropdownToggle, DropdownMenu} from 'reactstrap';
+import '../App.css';
+
+
+const FeedTable = ({ articles}) => {
+    const  [dropdownOpen, setOpen] = useState(false);
+    const toggle = () => setOpen(!dropdownOpen);
+
+    return(
+        <Table>
+            <thead>
+                <tr>
+                    <td>News Feed</td>
+                    <td colSpan="2">Filter</td>
+                    <td colSpan="2">Sort</td>
+                </tr>
+            </thead>
+            <tbody>
+             {(articles.length > 0) ? articles.map( (article) => {
+                   return (
+                    <tr>
+                        <td>{ article.name }</td>
+                        <td>{ article.date}</td>
+                        <td><Button color="primary" size="sm" onClick={() => window.open(`${article.url}`, "_blank")}>Read</Button></td>
+                        <td><Button color="info" size="sm">Preview</Button></td>
+                        <td><ButtonDropdown color="secondary" size="sm" isOpen={dropdownOpen} toggle={toggle}>
+                                <DropdownToggle caret>
+                                    Rate
+                                </DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem>
+                                        5
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        4
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        3
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        2
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        1
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </ButtonDropdown>
+                        </td>
+                    </tr>
+                   )
+               }) : <tr><td colSpan="5">Loading Data...</td></tr>}
+                         
+            </tbody>
+        </Table>
+    );
+}
+
+export default FeedTable;
--- client/src/components/news-list.component.js
+++ client/src/components/news-list.component.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-/* import { Link } from 'react-router-dom'; */
-
+import FeedTable from './feed-articles.component';
 import { Container, Row, Col } from 'reactstrap';
-
 import '../App.css';
+import RatedTable from './rated-articles.component';
+
 export default class NewsList extends Component {
     /*constructor(props) { super(props); } */
+    constructor(props){
+        super(props);
+        this.state = {
+            articles: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('../api/news')
+        .then(res => res.json())
+        .then(json => this.setState({ 'articles': json }))
+    }
+
+    
     render() {
+        
         return (
             <Container className='news-list' fluid>
                 <Row className='news-list-row'>
-                    <Col xs='4'className='news'>news</Col>
-                    <Col xs='4'className='abstract'>abstrac</Col>
-                    <Col className='recommended-news'>recommended news</Col>
+                    <Col xs='6'className='news'>
+                        <FeedTable articles={ this.state.articles } />
+                    </Col>
                 </Row>
             </Container>  
         );
