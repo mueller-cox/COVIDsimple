f8323b89de6c41eab43f02129f97b190bc40df61
Meghan Mueller-Cox <mlm25@pdx.edu>
2020-07-29 17:39:46 -0700

    adding filter by source to news feed in news-list

--- client/src/components/feed-articles.component.js
+++ client/src/components/feed-articles.component.js
@@ -9,25 +9,27 @@ const FeedTable = ({ articles }) => {
     const [currentPage, setPage] = useState(0);
     const pageSize=50;
     const pageCount = Math.floor((articles.length) / pageSize);
+
     return(
         <div>
         <Table>
             <thead>
                 <tr>
-                    <td>News Feed</td>
-                    <td colSpan="2">Filter</td>
+                    <td colSpan="2">
+                    </td>
+                    <td></td>
                     <td colSpan="2">Sort</td>
                 </tr>
             </thead>
             <tbody>
             {(articles.length > 0) ? articles
                 .slice(currentPage * pageSize, (currentPage + 1) * pageSize)
-                .map( (article, i) => {
+                .map( (articles, i) => {
                    return (
                     <tr key={ i }>
-                        <td>{ article.name }</td>
-                        <td>{ article.date }</td>
-                        <td><Button color="primary" size="sm" onClick={() => window.open(`${article.url}`, "_blank")}>Read</Button></td>
+                        <td>{ articles.name }</td>
+                        <td>{ articles.date }</td>
+                        <td><Button color="primary" size="sm" onClick={() => window.open(`${articles.url}`, "_blank")}>Read</Button></td>
                         <td><Button color="info" size="sm">Preview</Button></td>
                         <td><RatingDropDownButton/></td>
                     </tr>
