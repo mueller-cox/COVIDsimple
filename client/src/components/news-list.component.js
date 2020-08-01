f656ba71e27e5f37bd9bb14407484e6084d7a004
Meghan Mueller-Cox <mlm25@pdx.edu>
2020-08-01 15:49:56 -0700

    reformatted reactstrap in news so that when media is small two containers become vertically stacked and integrated rated articles component into news

--- client/src/components/news-list.component.js
+++ client/src/components/news-list.component.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import FeedTable from './feed-articles.component';
+import RatedTable from './rated-articles.component';
 import { Container, Row, Col } from 'reactstrap';
 import '../App.css';
-//import RatedTable from './rated-articles.component';
+
 
 export default class NewsList extends Component {
     /*constructor(props) { super(props); } */
@@ -10,50 +11,120 @@ export default class NewsList extends Component {
         super(props);
         this.state = {
             articles: [],
-            src_filter: "None"
+            news_src_filter: "None",
+            rated: [],
+            rated_src_filter: "None",
         }
     }
 
     async componentDidMount() {
-        let url = '../api/news';
+        let url_news = '../api/news';
+        let url_rated = '../api/articles';
         try {
-            let response = await fetch(url);
+            let response = await fetch(url_news);
             if (!response.ok) {
                 throw (response.error);
             }
             let json = await response.json();
-            this.setState({ 'articles': json, 'filterd_articles': json });
+            this.setState({ 'articles': json });
+        } catch(error) {
+            console.error(error);
+        }
+
+        try {
+            let response = await fetch(url_rated);
+            if (!response.ok) {
+                throw (response.error);
+            }
+            let json = await response.json();
+            this.setState({ 'rated': json});
         } catch(error) {
             console.error(error);
         }
     }
 
-    handleSrcFilter = (event) => {
-        this.setState({ src_filter: event.target.value })
+    handleNewsSrcFilter = (event) => {
+        this.setState({ news_src_filter: event.target.value })
+    }
+
+    handleRatedSrcFilter = (event) => {
+        this.setState({ rated_src_filter: event.target.value })
+    }
+
+    updateRated = async () => {
+        let url_rated = '../api/articles';
+        try {
+            let response = await fetch(url_rated);
+            if (!response.ok) {
+                throw (response.error);
+            }
+            let json = await response.json();
+            this.setState({ 'rated': json });
+        } catch(error) {
+            console.error(error);
+        }
+
     }
 
     
     render() {
         return (
-            <Container className='news-list' fluid>
-                <Row className='news-filter-row'>
-                    <Col xs='3' classname='filter'>
-                        <label for="filter_src">Filter by Source  </label>
-                        <select class='filter' id="filter_src" name="filter_src" onChange={this.handleSrcFilter}>
-                            <option value="None">None</option>
-                            <option value="World">WHO</option>
-                            <option value="Disease">CDC</option>
-                        </select>
+            <Container fluid>
+                <Row>
+                    <Col sm='12' md='6'>
+                        <Container className='news-list'>
+                            <Row className='header-row'>
+                                <Col >
+                                    <h3>Latest News from WHO and CDC</h3>
+                                </Col>
+                            </Row>
+                            <Row className='news-filter-row'>
+                                <Col classname='filter'>
+                                    <label htmlFor="news_filter_src">Filter by Source  </label>
+                                    <select className='filter' id="news_filter_src" name="news_filter_src" onChange={this.handleNewsSrcFilter}>
+                                        <option value="None">None</option>
+                                        <option value="World">WHO</option>
+                                        <option value="Disease">CDC</option>
+                                    </select>
+                                </Col>
+                            </Row>
+                            <Row className='news-list-row'>
+                                <Col className='news'>
+                                    <FeedTable articles={ this.state.news_src_filter === "None" ? this.state.articles : 
+                                                        this.state.articles.filter(article => article.source.includes(this.state.news_src_filter))} 
+                                    />
+                                </Col>
+                            </Row>
+                        </Container>
                     </Col>
+                    <Col > 
+                        <Container>
+                            <Row className='header-row'>
+                                <Col >
+                                    <h3>Rated News</h3>
+                                </Col>
+                            </Row>
+                            <Row className='news-filter-row'>
+                                <Col className='filter'>
+                                    <label htmlFor="rated_filter_src">Filter by Source  </label>
+                                    <select className='filter' id="rated_filter_src" name="rated_filter_src" onChange={this.handleRatedSrcFilter}>
+                                        <option value="None">None</option>
+                                        <option value="World">WHO</option>
+                                        <option value="Disease">CDC</option>
+                                    </select>
+                                </Col>
+                            </Row>
+                            <Row className='news-list-row'>
+                                <Col  className='news'>
+                                    <RatedTable articles={ this.state.rated_src_filter === "None" ? this.state.rated : 
+                                                        this.state.rated.filter(article => article.source.includes(this.state.rated_src_filter))}
+                                    />
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Col>   
                 </Row>
-                <Row className='news-list-row'>
-                    <Col xs='6'className='news'>
-                        <FeedTable articles={ this.state.src_filter === "None" ? this.state.articles : 
-                                              this.state.articles.filter(article => article.source.includes(this.state.src_filter))}
-                        />
-                    </Col>
-                </Row>
-            </Container>  
+            </Container>
         );
     }
 }
