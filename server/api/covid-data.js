b4143c8fba96e9008e698aa3750a9ef6f4df6803
Connor Robetorye <crobetorye@gmail.com>
2020-07-31 17:03:53 -0700

    upgrade from request-promise to axios

--- server/api/covid-data.js
+++ server/api/covid-data.js
@@ -4,14 +4,24 @@ var CovidData = require('../models/covid-data');
 var router = express.Router();
 
 /**
+ * state data request: localhost:5000/api/covid-data/states 
+ * fetches array of all historical data for every state, one entry/day/state */
+ router.get('/states', (req, res) => {
+
+  CovidData.retrieveStates((err, data) => {
+    if (err) { return res.json(err); }
+    return res.json(data);
+  });
+});
+
+/**
  * state data request: localhost:5000/api/covid-data/XX 
  * fetches array of all historical data for requested state, one entry/day */
 router.get('/:state', (req, res) => {
   var state = req.params.state;
 
   CovidData.retrieveByState(state, (err, data) => {
-    if (err) 
-      return res.json(err);
+    if (err){ return res.json(err); }
     return res.json(data);
   });
 });
@@ -21,8 +31,7 @@ router.get('/:state', (req, res) => {
  * fetches array of nationally accumulated data, one entry/day */
 router.get('/', (req, res) => {
   CovidData.retrieveNational((err, data) => {
-    if (err) 
-      return res.json(err);
+    if (err) { return res.json(err); }
     return res.json(data);
   });
 });
