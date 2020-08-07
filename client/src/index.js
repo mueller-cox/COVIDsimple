75ab98922a85d45d1c5ca9d2b815f785422d4f5c
Connor Robetorye <crobetorye@gmail.com>
2020-08-06 20:21:38 -0700

    turn off strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<React.StrictMode>
+    <App />,
+  //</React.StrictMode>,
   document.getElementById('root')
 );
 
