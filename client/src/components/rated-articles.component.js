f955bfa3854aa3443692cdcfe88736472b1e5544
Meghan Mueller-Cox <mlm25@pdx.edu>
2020-08-07 15:25:10 -0700

    added sort functionality to ratings table can sort on rating, date, and source

--- client/src/components/rated-articles.component.js
+++ client/src/components/rated-articles.component.js
@@ -4,6 +4,8 @@ import ReactHtmlParser from 'react-html-parser';
 
 import RatingDropDownButton from './rating.component';
 import PaginationTool from './table-pagination.component';
+import SortRatedButton from './sort-rated.component';
+
 import '../App.css';
 
 
@@ -13,14 +15,77 @@ const RatedTable = ({ articles }) => {
     const pageCount = Math.ceil((articles.length) / pageSize);
     const [startIndex, setStartIndex] = useState(0);
     const [endIndex, setEndIndex] = useState(5);
+    const [rated_sort, setRatedSort] = useState("date");
+    const [rated_sort_dir, setRatedSortDir] = useState("Desc");
+    /* compare values for sort, TODO refactor so rating is calculated and stored in DB
+       this is gross...
+    */
+    const compareValuesNotRating = (a, b) => {
+        if(rated_sort_dir === "Desc"){
+            if(b[rated_sort] < a[rated_sort]){
+                return -1
+            }
+            if( b[rated_sort] > a[rated_sort]){
+                return 1;
+            }
+            return 0;
+        }else{
+            if(b[rated_sort] > a[rated_sort]){
+                return -1
+            }
+            if( b[rated_sort] < a[rated_sort]){
+                return 1;
+            }
+            return 0;
+        }
+    }
+    /* this is really gross ..., see above*/
+    const compareValuesRating = (a, b) => {
+        let b_rating = Math.floor(b.rating_sum / b.rating_count);
+        let a_rating = Math.floor(a.rating_sum / a.rating_count);
+        if(rated_sort_dir === "Desc"){
+            if(b_rating < a_rating){
+                return -1
+            }
+            if( b_rating > a_rating){
+                return 1;
+            }
+            return 0;
+        }else{
+            if(b_rating > a_rating){
+                return -1
+            }
+            if(b_rating < a_rating){
+                return 1;
+            }
+            return 0;
+        }
+    }
 
     return(
         <div className="xs=5">
         <Table id='rated-news' name='rated-news' responsive borderless>
             <thead>
+                <tr>
+                    <th colSpan="2">
+                        <SortRatedButton 
+                             handleSortClick={(e, filter) =>{
+                                 if(filter === rated_sort && rated_sort_dir === "Desc"){
+                                     setRatedSortDir("Asc");
+                                 }
+                                 else{
+                                     setRatedSortDir("Desc");
+                                 }
+                                 setRatedSort(filter);
+                             }}
+                        />
+                         <span className="sorted-on-text"> Sorted: {rated_sort.charAt(0).toUpperCase()+rated_sort.slice(1)} {rated_sort_dir} </span>
+                    </th>
+                </tr>    
             </thead>
             <tbody>
              {(articles.length > 0) ? articles
+                .sort(rated_sort === 'rating' ? compareValuesRating : compareValuesRating)
                 .slice(currentPage * pageSize, (currentPage + 1) * pageSize)
                 .map( (article, index) => {
                    return (
