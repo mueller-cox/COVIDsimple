6dbf34ab5aedc54743bb143dccecd5cb1cc7737b
Connor Robetorye <crobetorye@gmail.com>
2020-08-08 15:18:10 -0700

    404 page redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 // How to import relevant component from the module for reactstrap
 // import { Container, Row, Col} from 'reactstrap';
 
@@ -10,7 +10,7 @@ import StateView from "./components/state-view.component";
 import NewsList from "./components/news-list.component";
 import About from "./components/about.component";
 import WhatIsCovid from "./components/what-is-covid19.component";
-// import noMatch from "./components/404page.component";
+import noMatch from "./components/404page.component";
 
 
 
@@ -18,13 +18,15 @@ function App() {
   return (
     <Router>
       <NavMenu />
-      <br />
-      <Route path="/" exact component={NationalView} />
-      <Route path="/state-view" exact component={StateView} />
-      <Route path="/news" exact component={NewsList} />
-      <Route path="/about" exact component={About} />
-      <Route path="/What-is-covid19" exact component={WhatIsCovid} />
-      {/* <Route path="/404page" exact component={noMatch} /> */}
+      <Switch>
+        <Route path="/" exact component={NationalView} />
+        <Route path="/state-view" exact component={StateView} />
+        <Route path="/news" exact component={NewsList} />
+        <Route path="/about" exact component={About} />
+        <Route path="/What-is-covid19" exact component={WhatIsCovid} />
+        <Route path="/404" exact component={noMatch} />
+        <Redirect to="/404"/>
+      </Switch>
     </Router>
   );
 }
--- client/src/components/404page.component.js
+++ client/src/components/404page.component.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import {
     Card,
-    CardText,
+    /*CardText,*/
     CardBody,
     CardTitle,
     CardSubtitle
@@ -14,8 +14,8 @@ export default class noMatch extends Component {
         return (
             <Card className="noMatch-body">
                 <CardBody className="noMatch-body">
-                    <CardTitle className="noMatch-title">404 Page</CardTitle>
-                    <CardSubtitle className="noMatch-subtitle">APIs error</CardSubtitle>
+                    <CardTitle className="noMatch-title">Error 404: page not found</CardTitle>
+                    <CardSubtitle className="noMatch-subtitle">Please use the navbar above to redirect</CardSubtitle>
                     {/* <CardText className="noMatch-text">Clicking the link below will redirect you to the News Page</CardText> */}
                     {/* <p className="match-redirect">Redirecting to */}
                     {/* <spam onClick={() => history.push('/news')}> */}
