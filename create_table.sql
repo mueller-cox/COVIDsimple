0d92305b8df82840bcbc7dd68502bae55bcd6e15
Meghan Mueller-Cox <mlm25@pdx.edu>
2020-08-08 16:29:56 -0700

    added missing primary key to sql create file

--- create_table.sql
+++ create_table.sql
@@ -1,10 +1,11 @@
-CREATE TABLE public.articles (
+CREATE TABLE articles (
     source character varying(30) NOT NULL,
     url character varying NOT NULL,
     name character varying,
     date character varying,
     rating_count integer,
-    rating_sum integer
+    rating_sum integer,
+    PRIMARY KEY (url)
 );
 
 
